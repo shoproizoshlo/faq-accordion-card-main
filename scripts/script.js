6787783484d76c073146840b87403248ed367ca5
shoproizoshlo <iamsusunny@gmail.com>
2023-09-07 14:57:12 +0200

    another one fix to the toggle buttons

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,6 @@ toggleButtons.forEach((button) =>
     const targetId = button.getAttribute("data-target");
     const targetElement = document.getElementById(targetId);
 
-    // if (
-    //   targetElement.style.display === "none" ||
-    //   targetElement.style.display === ""
-    // ) {
-    //   targetElement.style.display = "block";
-    //   button.style.transform = "rotate(180deg)";
-    //   button.style.transition = "transform 0.5s ease";
-    // } else {
-    //   targetElement.style.display = "none";
-    //   button.style.transform = "rotate(0deg)";
-    //   button.style.transition = "transform 0.5s ease";
-    // }
-
     if (targetElement.classList.contains("open")) {
       targetElement.classList.remove("open");
       button.style.transform = "rotate(0deg)";
@@ -27,12 +14,5 @@ toggleButtons.forEach((button) =>
       button.style.transform = "rotate(180deg)";
       button.style.transition = "transform 0.5s ease";
     }
-    //   if (targetElement.classList.contains("open")) {
-    //   targetElement.classList.remove("open");
-    //   button.classList.remove("rotated");
-    // } else {
-    //   targetElement.classList.add("open");
-    //   button.classList.add("rotated");
-    // }
   })
 );
