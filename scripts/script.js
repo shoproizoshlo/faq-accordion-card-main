53cd161a32f8b879b4dedb4163a65a97f1d23031
shoproizoshlo <iamsusunny@gmail.com>
2023-09-07 15:59:17 +0200

    fixed toggle button

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,41 @@
-var toggleButtons = document.querySelectorAll(".icon-arrow-down");
+const toggleButtons = document.querySelectorAll(".icon-arrow-down");
+const toggleTexts = document.querySelectorAll(".toggle-text");
+var currentOpenButton = null;
 
 toggleButtons.forEach((button) =>
   button.addEventListener("click", () => {
     const targetId = button.getAttribute("data-target");
     const targetElement = document.getElementById(targetId);
 
-    document.querySelectorAll(".toggle-text").forEach((text) => {
-      if (text.id !== targetId) {
-        text.classList.remove("open");
-      }
-    });
+    if (currentOpenButton && currentOpenButton !== button) {
+      const currentTargetId = currentOpenButton.getAttribute("data-target");
+      const currentTargetElement = document.getElementById(currentTargetId);
 
+      currentTargetElement.classList.remove("open");
+      currentOpenButton.classList.remove("closing");
+      currentOpenButton.classList.remove("rotated");
+    }
+
+    // Закрываем текущий текст
     if (targetElement.classList.contains("open")) {
       targetElement.classList.remove("open");
-      button.style.transform = "rotate(0deg)";
-      button.style.transition = "transform 0.5s ease";
+      button.classList.remove("closing");
     } else {
       targetElement.classList.add("open");
-      button.style.transform = "rotate(180deg)";
-      button.style.transition = "transform 0.5s ease";
+      button.classList.add("closing");
     }
+
+    // Обновляем текущую открытую стрелку
+    currentOpenButton = button;
+
+    // if (targetElement.classList.contains("open")) {
+    //   targetElement.classList.remove("open");
+    //   button.style.transform = "rotate(0deg)";
+    //   button.style.transition = "transform 0.5s ease";
+    // } else {
+    //   targetElement.classList.add("open");
+    //   button.style.transform = "rotate(180deg)";
+    //   button.style.transition = "transform 0.5s ease";
+    // }
   })
 );
