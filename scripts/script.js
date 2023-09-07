0695388777ef353481e5e9a46a65e51221bc0ad9
shoproizoshlo <iamsusunny@gmail.com>
2023-09-07 14:29:24 +0200

    animated arrows

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,18 @@ toggleButtons.forEach((button) =>
   button.addEventListener("click", () => {
     const targetId = button.getAttribute("data-target");
     const targetElement = document.getElementById(targetId);
-    if (targetElement.style.display === "none") {
+
+    if (
+      targetElement.style.display === "none" ||
+      targetElement.style.display === ""
+    ) {
       targetElement.style.display = "block";
+      button.style.transform = "rotate(180deg)";
+      button.style.transition = "transform 0.5s ease";
     } else {
       targetElement.style.display = "none";
+      button.style.transform = "rotate(0deg)";
+      button.style.transition = "transform 0.5s ease";
     }
   })
 );
-
-const iconArrows = document.querySelectorAll(".icon-arrow-down");
