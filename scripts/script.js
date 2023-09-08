32e7680d00c47e15e9e6629f8b873df2b5905e20
shoproizoshlo <iamsusunny@gmail.com>
2023-09-08 12:00:20 +0200

    arrow rotate

--- scripts/script.js
+++ scripts/script.js
@@ -15,16 +15,20 @@ toggleLine.forEach((line, index) => {
     questionTexts.forEach((question) => {
       question.classList.remove("bold");
     });
+    toggleButtons.forEach((button) => {
+      button.classList.remove("closing");
+    });
 
     // Проверяем, если есть текущая открытая строка
     if (currentOpenLine && currentOpenLine !== line) {
       const currentTargetId = currentOpenLine.getAttribute("data-target");
       const currentTargetElement = document.getElementById(currentTargetId);
       const currentQuestionText = questionTexts[index]; // Получаем соответствующий элемент с классом "question"
+      const currentToggleButtons = toggleButtons[index]; // Получаем соответствующий элемент с классом "icon-arrow-down"
 
       currentTargetElement.classList.remove("open");
       currentOpenLine.classList.remove("closing");
-      currentOpenLine.classList.remove("rotated");
+      currentToggleButtons.classList.remove("rotated");
       currentQuestionText.classList.remove("bold"); // Убираем выделение жирным
     }
 
@@ -53,31 +57,12 @@ toggleLine.forEach((line, index) => {
     }
 
     questionTexts[index].classList.toggle("bold");
+    toggleButtons[index].classList.toggle("closing");
 
     // Обновляем текущую открытую стрелку
     currentOpenLine = line;
   });
 });
-
-//   toggleButtons.forEach((button, index) =>
-//     button.addEventListener("click", () => {
-
-//       // Проверяем, если есть текущая открытая стрелка
-//       if (currentOpenButton && currentOpenButton !== button) {
-//         const currentTargetId =
-//           currentOpenButton.getAttribute("data-target");
-//         const currentTargetElement =
-//           document.getElementById(currentTargetId);
-//         const currentQuestionText = questionTexts[index]; // Получаем соответствующий элемент с классом "question"
-
-//         currentTargetElement.classList.remove("open");
-//         currentOpenButton.classList.remove("closing");
-//         currentOpenButton.classList.remove("rotated");
-//         currentQuestionText.classList.remove("bold"); // Убираем выделение жирным
-//       }
-
-//       // Обновляем текущую открытую стрелку
-//       currentOpenButton = button;
 
 //
 
