62bb55e9ca54e424f00cf6d53ab43be73b4cc215
shoproizoshlo <iamsusunny@gmail.com>
2023-09-08 14:46:57 +0200

    cleaned files

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const toggleButtons = document.querySelectorAll(".icon-arrow-down");
 const toggleLine = document.querySelectorAll(".q-line");
 const toggleTexts = document.querySelectorAll(".toggle-text");
-const questionTexts = document.querySelectorAll(".question"); // Находим все элементы с классом "question"
+const questionTexts = document.querySelectorAll(".question");
 
 let currentOpenButton = null;
 let currentOpenLine = null;
@@ -16,22 +16,20 @@ toggleLine.forEach((line, index) => {
       question.classList.remove("bold");
     });
 
-    // Проверяем, если есть текущая открытая строка
     if (currentOpenLine && currentOpenLine !== line) {
       const currentTargetId = currentOpenLine.getAttribute("data-target");
       const currentTargetElement = document.getElementById(currentTargetId);
-      const currentQuestionText = questionTexts[index]; // Получаем соответствующий элемент с классом "question"
-      const currentToggleButtons = toggleButtons[index]; // Получаем соответствующий элемент с классом "icon-arrow-down"
+      const currentQuestionText = questionTexts[index];
+      const currentToggleButtons = toggleButtons[index];
 
       currentTargetElement.classList.remove("open");
       currentOpenLine.classList.remove("closing");
-      currentQuestionText.classList.remove("bold"); // Убираем выделение жирным
+      currentQuestionText.classList.remove("bold");
       toggleButtons.forEach((button) => {
         button.classList.remove("closing");
       });
     }
 
-    // Закрываем текущий текст
     if (targetElement.classList.contains("open")) {
       targetElement.classList.remove("open");
     } else {
@@ -41,47 +39,6 @@ toggleLine.forEach((line, index) => {
     questionTexts[index].classList.toggle("bold");
     toggleButtons[index].classList.toggle("closing");
 
-    // Обновляем текущую открытую строку
     currentOpenLine = line;
   });
 });
-
-//
-
-//
-
-// toggleButtons.forEach((button, index) =>
-//   button.addEventListener("click", () => {
-//     const targetId = button.getAttribute("data-target");
-//     const targetElement = document.getElementById(targetId);
-
-//     questionTexts.forEach((question) => {
-//       question.classList.remove("bold");
-//     });
-
-//     // Проверяем, если есть текущая открытая стрелка
-//     if (currentOpenButton && currentOpenButton !== button) {
-//       const currentTargetId = currentOpenButton.getAttribute("data-target");
-//       const currentTargetElement = document.getElementById(currentTargetId);
-//       const currentQuestionText = questionTexts[index]; // Получаем соответствующий элемент с классом "question"
-
-//       currentTargetElement.classList.remove("open");
-//       currentOpenButton.classList.remove("closing");
-//       currentOpenButton.classList.remove("rotated");
-//       currentQuestionText.classList.remove("bold"); // Убираем выделение жирным
-//     }
-
-//     // Закрываем текущий текст
-//     if (targetElement.classList.contains("open")) {
-//       targetElement.classList.remove("open");
-//       button.classList.remove("closing");
-//     } else {
-//       targetElement.classList.add("open");
-//       button.classList.add("closing");
-//     }
-//     questionTexts[index].classList.toggle("bold");
-
-//     // Обновляем текущую открытую стрелку
-//     currentOpenButton = button;
-//   })
-// );
